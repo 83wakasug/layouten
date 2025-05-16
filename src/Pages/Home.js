940ae6db95fd6a363fb1d6b565c146d5d1b7b7bc
Wakana <pre.kuro.05@gmail.com>
2025-05-16 09:53:46 +0200

    add keitai

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ import PostList from '../Component/PostList';
 const Home = () => {
   return (
     
-    <div className="flex flex-row">
+    <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:flex-row">
      <PostList />
      <TagList />
       
